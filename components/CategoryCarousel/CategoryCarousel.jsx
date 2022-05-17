af20227b7d9ef33052c3241e9526af09f2512e0b
Lew Yuburi <lewisyubu@gmail.com>
2022-05-16 21:31:23 -0500

    tienda EPSON, 3M

---
+++ components/CategoryCarousel/CategoryCarousel.jsx
@@ -0,0 +1,54 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const Carousel = (props) => {
+	const { productCategories } = props;
+
+	return (
+		<Swiper
+			pagination={{
+				clickable: true,
+			}}
+			slidesPerView={1.5}
+			spaceBetween={20}
+			breakpoints={{
+				768: {
+					slidesPerView: 4,
+					spaceBetween: 40,
+				},
+				1024: {
+					slidesPerView: 5,
+					spaceBetween: 50,
+				},
+			}}>
+			{productCategories.map((category) => (
+				<SwiperSlide key={category.label}>
+					<Box component='a' href={category.url}>
+						<Box
+							sx={{
+								'&>img': {
+									width: '100%',
+									position: 'relative',
+									display: 'block',
+									margin: '0 auto',
+								},
+							}}>
+							<img src={category.img} alt={category.label} />
+						</Box>
+						<Typography
+							textAlign='center'
+							color='text.secondary'
+							fontWeight='500'>
+							{category.label}
+						</Typography>
+					</Box>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
+
+export default Carousel;
---
+++ pages/newell/index.jsx
@@ -0,0 +1,113 @@
+import Box from '@mui/material/Box';
+
+import { Layout } from 'layout/Layout';
+import HeroImage from 'components/HeroImage';
+import CategoryGrid from 'components/CategoryGrid';
+import BrandCarousel from 'components/BrandCarousel';
+
+const productCategories = [
+	{
+		label: 'Plumas',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/papermate.png',
+		url: '/busquedas.asp?/PRODUCTOS-PARA-OFICINA/ESCRITURA/BOLIGRAFO&m=BEROL',
+	},
+	{
+		label: 'Colores',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/prismacolor.png',
+		url: '/busquedas.asp?/PRODUCTOS-PARA-OFICINA/ESCRITURA/LAPICES-DE-COLOR&m=BEROL',
+	},
+	{
+		label: 'Marcadores',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/sharpie.png',
+		url: '/busquedas.asp?/PRODUCTOS-PARA-OFICINA/ESCRITURA/MARCADORES-Y-MARCATEXTOS&m=BEROL',
+	},
+	{
+		label: 'Pegamento',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/elmers.png',
+		url: '/busquedas.asp?/PRODUCTOS-PARA-OFICINA/ADHESIVOS&m=BEROL',
+	},
+	{
+		label: 'Lapices',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/lapices.png',
+		url: '/busquedas.asp?query=lapiz&m=BEROL',
+	},
+	{
+		label: 'Cintas',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/dymo.png',
+		url: '/busquedas.asp?query=DYMO&m=BEROL',
+	},
+	{
+		label: 'Marcatextos',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/marcatextos.png',
+		url: '/busquedas.asp?query=marcatextos&m=BEROL',
+	},
+	{
+		label: 'Corrector',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/berol.png',
+		url: '/busquedas.asp?/PRODUCTOS-PARA-OFICINA/ESCRITURA/CORRECTORES&m=BEROL',
+	},
+];
+
+const brands = [
+	{
+		label: 'Berol',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/berol-1.png',
+		url: '/busquedas.asp?query=Berol',
+	},
+	{
+		label: 'Sharpie',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/sharpie.png',
+		url: '/busquedas.asp?query=Sharpie',
+	},
+	{
+		label: 'Prismacolor',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/prismacolor.png',
+		url: '/busquedas.asp?query=Prismacolor',
+	},
+	{
+		label: 'Paper-mate',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/paper-mate.png',
+		url: '/busquedas.asp?query=Paper-mate',
+	},
+	{
+		label: 'Uniball',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/uniball.png',
+		url: '/busquedas.asp?query=Uniball',
+	},
+	{
+		label: 'Dymo',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/dymo.png',
+		url: '#',
+	},
+	{
+		label: 'Elmers',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/elmers.png',
+		url: '/busquedas.asp?query=Elmers',
+	},
+	{
+		label: 'Expo',
+		img: 'https://pedidos.com/myfotos/pedidos-com/pagina/newell/marcas/expo.png',
+		url: '/busquedas.asp?query=EXPO',
+	},
+];
+
+const Newell = () => {
+	return (
+		<Layout>
+			<HeroImage
+				src={require('../../public/images/landing/newellback1.png')}
+				alt='Newell'
+			/>
+
+			<Box>
+				<BrandCarousel brands={brands} />
+			</Box>
+
+			<Box pt={3} px={8} pb={6}>
+				<CategoryGrid categories={productCategories} />
+			</Box>
+		</Layout>
+	);
+};
+
+export default Newell;
