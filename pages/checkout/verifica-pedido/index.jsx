d147837179eb84a2de8475e1ee11b320eb9ca31c
karla <diseo pedidos@dcm.com.mx>
2022-06-22 19:18:04 -0500

    se ajusta theme

--- pages/checkout/verifica-pedido/index.jsx
+++ pages/checkout/verifica-pedido/index.jsx
@@ -612,7 +612,7 @@ export default function Verifica_pedido() {
 										spacing={2}>
 										<Grid item xs={12}>
 											<Box component='div' m={1} pt={4}>
-												<img sx={{padding: theme.spacing(1), width: '160px',}}
+												<img sx={{ width: '160px'}}
 													src='https://pedidos.com/myfotos/pedidos-com/pagina/carrito-compra/carrito-v.svg'
 													alt='Carrito vacio'
 												/>
