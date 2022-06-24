8fa2e732ed79730451a5a9a37433c6f912ad8fa1
karla <diseo pedidos@dcm.com.mx>
2022-06-23 19:29:27 -0500

    ajuste de error

--- pages/next-sitemap.js
+++ pages/next-sitemap.js
@@ -1,6 +1,13 @@
-const siteUrl = "https://pedidos.com/";
+export default config
 
-module.exports = {
-    siteUrl,
-    generateRobotstxt: true,
-}
+const config = {
+    siteUrl: process.env.SITE_URL || 'https://pedidos.com/',
+    generateRobotsTxt: true,
+    // optional
+    //robotsTxtOptions: {
+    //  additionalSitemaps: [
+    //   'https://pedidos.com/sitemap-1.xml',
+    //    'https://pedidos.com/sitemap-2.xml',
+    //  ],
+    //},
+  }
