b855628f24d7ef9b62517193ce1c4705ab8e6c62
GiovaniPedidos <giovani.j@pedidos.com.mx>
2022-06-30 18:46:14 -0500

    Actualizacion de banner home

--- components/home/mainSlideShow.jsx
+++ components/home/mainSlideShow.jsx
@@ -50,32 +50,7 @@ export default function MainSlideShow() {
 			className="mySwiper"
 			onSlideChange={() => console.log('slide change')}
 			onSwiper={(swiper) => console.log(swiper)}>
-				
-			<SwiperSlide> 
-				<Box component="div" sx={{position:'relative'}}>
-					<Link href='/articulos/imac-2017-apple-mhk03ea-intel-core-i5-8-gb-ssd-256-gb-led-21-5-pulgadas-mac-os'>
-						<a>	
-							<Box component="div">
-								<Hidden mdDown>
-									<Banner xs={{width:'100%'}} props={{
-									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/imac-aar.webp',
-									alt:'Apple, iMac',
-									
-									}}
-									/>
-								</Hidden>
-								<Hidden mdUp>
-									<Banner props={{
-									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/imac-xs-june.png',
-									alt:'Apple, iMac',
-									}}
-									/>
-								</Hidden>
-							</Box>	
-						</a>
-					</Link>
-				</Box>
-			</SwiperSlide>
+
 			<SwiperSlide> 
 				<Link href='/articulos/laptop-hp-14-dk1015la-procesador-amd-athlon-silver-3050u-ram-4-gb-dd-256-gb-windows-10-home-64-pantalla-hd-14-pulgadas-'>
 					<a>
@@ -143,6 +118,166 @@ export default function MainSlideShow() {
 					</a>
 				</Link>
 			</SwiperSlide>
+
+			<SwiperSlide> 
+				<Link href='/articulos/laptop-hp-14-dk1015la-procesador-amd-athlon-silver-3050u-ram-4-gb-dd-256-gb-windows-10-home-64-pantalla-hd-14-pulgadas-'>
+					<a>
+						<Box component="div" sx={{position:'relative'}}>
+							<Box component="div">
+								<Hidden mdDown>
+									<Box component="div">
+										<Banner xs={{width:'100%'}} props={{
+										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/bro-30-desktop.webp',
+										alt:'hp, tecnologia, computo',
+										
+										}}
+										/>
+									</Box>
+								</Hidden>
+								<Hidden mdUp>
+									<Box component="div">
+										<Banner props={{
+										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/bro-30-sm-xs.webp',
+										alt:'hp, tecnologia, computo',
+										}}
+										/>
+									</Box>
+								</Hidden>
+							</Box>	
+							<Box component="div" className={styles.infopbox}>
+								<Grid spacing={1}
+								container
+								direction="row"
+								justifyContent="space-between"
+								alignItems="center">
+									<Grid item xs={4} sm={4}>
+										<Box component="div" sx={{position:'relative'}} >
+											<Paper variant='auto' px={1}  className={styles.boxmsi } >
+												<Box component="div" textAlign="center" py={1} px={3}> 
+													<Typography variant="overline" display="block">Hasta</Typography>
+														<Typography  variant="h4" component="body1" sx={{fontWeight:'600'}}>
+														<Box component="span" sx={{color:'#3655a5'}}>24</Box></Typography>
+														<Typography variant="caption" display="block" gutterBottom color="textSecondary">Tarjetas participantes*</Typography>
+														
+												</Box>
+											</Paper>
+										</Box>
+									</Grid>
+									<Divider orientation="vertical" flexItem />
+									<Grid item xs={7} sm={7}>
+										<Box component="div" >
+											<div>
+												<Typography className={styles.title} variant="h4" component="body1" > 
+												Desde <Box component="span" sx={{color:'#3655a5'}}>$4,299</Box>
+												</Typography>
+
+												<Typography variant="Subtitle1" component="p" >
+												Multifuncional brother DCPT520W
+												</Typography> 
+												<Typography variant="Subtitle1" component="p" color="textSecondary" >	
+												Tinta continua color
+												</Typography>  
+											</div>
+										</Box>
+									</Grid>
+								</Grid>
+							</Box>
+						</Box>
+					</a>
+				</Link>
+			</SwiperSlide>
+			<SwiperSlide> 
+				<Box component="div" sx={{position:'relative'}}>
+					<Link href='/articulos/imac-2017-apple-mhk03ea-intel-core-i5-8-gb-ssd-256-gb-led-21-5-pulgadas-mac-os'>
+						<a>	
+							<Box component="div">
+								<Hidden mdDown>
+									<Banner xs={{width:'100%'}} props={{
+									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/imac-aar.webp',
+									alt:'Apple, iMac',
+									
+									}}
+									/>
+								</Hidden>
+								<Hidden mdUp>
+									<Banner props={{
+									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/imac-xs-june.png',
+									alt:'Apple, iMac',
+									}}
+									/>
+								</Hidden>
+							</Box>	
+						</a>
+					</Link>
+				</Box>
+			</SwiperSlide>
+			<SwiperSlide> 
+				<Link href='/busquedas?query=proyector+epson'>
+					<a>
+						<Box component="div" sx={{position:'relative'}}>
+							<Box component="div">
+								<Hidden mdDown>
+									<Box component="div">
+										<Banner xs={{width:'100%'}} props={{
+										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/e20-desktop.webp',
+										alt:'hp, tecnologia, computo',
+										
+										}}
+										/>
+									</Box>
+								</Hidden>
+								<Hidden mdUp>
+									<Box component="div">
+										<Banner props={{
+										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/e20-sm-xs.webp',
+										alt:'hp, tecnologia, computo',
+										}}
+										/>
+									</Box>
+								</Hidden>
+							</Box>	
+							<Box component="div" className={styles.infopbox}>
+								<Grid spacing={1}
+								container
+								direction="row"
+								justifyContent="space-between"
+								alignItems="center">
+									<Grid item xs={4} sm={4}>
+										<Box component="div" sx={{position:'relative'}} >
+											<Paper variant='auto' px={1}  className={styles.boxmsi } >
+												<Box component="div" textAlign="center" py={1} px={3}> 
+													<Typography variant="overline" display="block">Hasta</Typography>
+														<Typography  variant="h4" component="body1" sx={{fontWeight:'600'}}>
+														<Box component="span" sx={{color:'#3655a5'}}>18</Box></Typography>
+														<Typography variant="caption" display="block" gutterBottom color="textSecondary">Tarjetas participantes*</Typography>
+														
+												</Box>
+											</Paper>
+										</Box>
+									</Grid>
+									<Divider orientation="vertical" flexItem />
+									<Grid item xs={7} sm={7}>
+										<Box component="div" >
+											<div>
+												<Typography className={styles.title} variant="h4" component="body1" > 
+												Desde <Box component="span" sx={{color:'#3655a5'}}>$10,699</Box>
+												</Typography>
+
+												<Typography variant="Subtitle1" component="p" >
+												Proyector Epson 
+												</Typography> 
+												<Typography variant="Subtitle1" component="p" color="textSecondary" >	
+												POWERLITE E20
+												</Typography>  
+											</div>
+										</Box>
+									</Grid>
+								</Grid>
+							</Box>
+						</Box>
+					</a>
+				</Link>
+			</SwiperSlide>
 			<SwiperSlide> 
 				<Link href='/apple/para-empresas'>
 					<a>
@@ -166,69 +301,6 @@ export default function MainSlideShow() {
 						</Box>
 					</a>
 				</Link>
-			</SwiperSlide>
-			<SwiperSlide> 
-				<Box component="div" sx={{position:'relative'}}>
-					<Link href='/busquedas?query=HTP200520211'>
-						<a>
-							<Box component="div">
-								<Hidden mdDown>
-									<Banner xs={{width:'100%'}} props={{
-									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/laptop-ideapad-lenovo-june-1.png',
-									alt:'Lenovo',
-									
-									}}
-									/>
-								</Hidden>
-								<Hidden mdUp>
-									<Banner props={{
-									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/laptop-ideapad-lenovo-june.png',
-									alt:'Lenovo',
-									}}
-									/>
-								</Hidden>
-							</Box>	
-							<Box component="div" className={styles.infopbox}>
-								<Grid spacing={1}
-								container
-								direction="row"
-								justifyContent="space-between"
-								alignItems="center">
-									<Grid item xs={4} sm={4}>
-										<Box component="div" sx={{position:'relative'}} >
-											<Paper variant='auto' px={1}  className={styles.boxmsi } >
-												<Box component="div" textAlign="center" py={1} px={3}> 
-													<Typography variant="overline" display="block">Hasta</Typography>
-														<Typography  variant="h4" component="body1" sx={{fontWeight:'600'}}>
-														<Box component="span" sx={{color:'#3655a5'}}>24</Box></Typography>
-														<Typography variant="caption" display="block" gutterBottom color="textSecondary">Tarjetas Citibanamex*</Typography>
-														
-												</Box>
-											</Paper>
-										</Box>
-									</Grid>
-									<Divider orientation="vertical" flexItem />
-									<Grid item xs={7} sm={7}>
-										<Box component="div" >
-											<div>
-												<Typography className={styles.title} variant="h4" component="body1" > 
-												Desde <Box component="span" sx={{color:'#3655a5'}}>$16,799</Box>
-												</Typography>
-
-												<Typography variant="Subtitle1" component="p" >
-													Lenovo IdeaPad 3 15ALC6
-												</Typography> 
-												<Typography variant="Subtitle1" component="p" color="textSecondary" >								
-													Ryzen 7, 16 GB, SSD 512 GB
-												</Typography>  
-											</div>
-										</Box>
-									</Grid>
-								</Grid>
-							</Box>
-						</a>
-					</Link>
-				</Box>
 			</SwiperSlide>
 			<SwiperSlide> 
 				<Link href='/busquedas?query=HTP200520221'>
@@ -298,14 +370,14 @@ export default function MainSlideShow() {
 				</Link>
 			</SwiperSlide>
 			<SwiperSlide> 
-				<Link href='/busquedas?query=HTP200520229'>
+				<Link href='/articulos/papel-ecobond-carta-70g-blancura-95'>
 					<a>
 						<Box component="div" sx={{position:'relative'}}>
 							<Box component="div">
 								<Hidden mdDown>
 									<Box component="div">
 										<Banner xs={{width:'100%'}} props={{
-										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/epson-1-desktop.webp',
+										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/ecobond-30-desktop.webp',
 										alt:'epson, tecnologia, impresion',
 										
 										}}
@@ -346,14 +418,14 @@ export default function MainSlideShow() {
 										<Box component="div" >
 											<div>
 												<Typography className={styles.title} variant="h4" component="body1" > 
-												Desde <Box component="span" sx={{color:'#3655a5'}}>$6,899</Box>
+												Desde <Box component="span" sx={{color:'#3655a5'}}>$1,079</Box>
 												</Typography>
 
 												<Typography variant="Subtitle1" component="p" >
-												Epson EcoTank L5290
+												Papel ECOBOND
 												</Typography> 
 												<Typography variant="Subtitle1" component="p" color="textSecondary" >		
-												Inyección de tinta
+												Tamaño Carta, Blancura 95%
 												</Typography>  
 											</div>
 										</Box>
@@ -364,31 +436,69 @@ export default function MainSlideShow() {
 					</a>
 				</Link>
 			</SwiperSlide>
-				<SwiperSlide> 
-					<Box component="div" sx={{position:'relative'}}>
-						<Link href='/busquedas?query=cartucho+de+tinta+hp'>
-							<a>	
-								<Box component="div">
-									<Hidden mdDown>
-										<Banner xs={{width:'100%'}} props={{
-										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/hp-consumibles.webp',
-										alt:'hp, tecnologia, impresion',
-										
-										}}
-										/>
-									</Hidden>
-									<Hidden mdUp>
-										<Banner props={{
-										img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/hp-consumibles-xs.webp',
-										alt:'hp, tecnologia, impresion',
-										}}
-										/>
-									</Hidden>
-								</Box>	
-							</a>
-						</Link>
-					</Box>
-				</SwiperSlide>
+			<SwiperSlide> 
+				<Box component="div" sx={{position:'relative'}}>
+					<Link href='/busquedas?query=HTP200520211'>
+						<a>
+							<Box component="div">
+								<Hidden mdDown>
+									<Banner xs={{width:'100%'}} props={{
+									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/len-30-desktop.webp',
+									alt:'Lenovo',
+									
+									}}
+									/>
+								</Hidden>
+								<Hidden mdUp>
+									<Banner props={{
+									img:'https://pedidos.com/myfotos/pedidos-com/pagina/home22/banners/len-30-sm-xs.webp',
+									alt:'Lenovo',
+									}}
+									/>
+								</Hidden>
+							</Box>	
+							<Box component="div" className={styles.infopbox}>
+								<Grid spacing={1}
+								container
+								direction="row"
+								justifyContent="space-between"
+								alignItems="center">
+									<Grid item xs={4} sm={4}>
+										<Box component="div" sx={{position:'relative'}} >
+											<Paper variant='auto' px={1}  className={styles.boxmsi } >
+												<Box component="div" textAlign="center" py={1} px={3}> 
+													<Typography variant="overline" display="block">Hasta</Typography>
+														<Typography  variant="h4" component="body1" sx={{fontWeight:'600'}}>
+														<Box component="span" sx={{color:'#3655a5'}}>18</Box></Typography>
+														<Typography variant="caption" display="block" gutterBottom color="textSecondary">Tarjetas Citibanamex*</Typography>
+														
+												</Box>
+											</Paper>
+										</Box>
+									</Grid>
+									<Divider orientation="vertical" flexItem />
+									<Grid item xs={7} sm={7}>
+										<Box component="div" >
+											<div>
+												<Typography className={styles.title} variant="h4" component="body1" > 
+												Desde <Box component="span" sx={{color:'#3655a5'}}>$15,899</Box>
+												</Typography>
+
+												<Typography variant="Subtitle1" component="p" >
+													Lenovo Think Pad E14
+												</Typography> 
+												<Typography variant="Subtitle1" component="p" color="textSecondary" >								
+													Ryzen 5, 8 GB, SSD 256 GB
+												</Typography>  
+											</div>
+										</Box>
+									</Grid>
+								</Grid>
+							</Box>
+						</a>
+					</Link>
+				</Box>
+			</SwiperSlide>
 			
 		</Swiper>
 	)
